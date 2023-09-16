5582288e40cd52ea46c5fc99a70f5fce1726c960
umair khan <uk473267@gmail.com>
2023-09-16 04:21:26 -0700

    init

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+     [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Title { get; set; }
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+    public string? Rating { get; set; }
+}
